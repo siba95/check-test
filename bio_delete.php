0df1b0b8e70012240f822a118a7dc365adde2755
siba95 <siba9525@yahoo.co.jp>
2016-05-24 13:27:20 +0900

    Add files via upload

---
+++ bio_delete.php
@@ -0,0 +1,13 @@
+<?php
+if(!empty($_POST["delete"]))
+{
+	$userId = htmlspecialchars($_POST["userId"], ENT_QUOTES, "UTF-8");
+	$bioId =htmlspecialchars($_POST["bioId"], ENT_QUOTES, "UTF-8");
+	$sql = "DELETE FROM user_bio WHERE bio_id = '$bioId'";
+
+	require_once 'db_connect.php';
+	$dbClass = new Postgres();
+	$userResult = $dbClass->deleteBio($sql);
+
+}
+?>
